d5fda4a4a747e871898ca18b291584ad64bc7ad0
sugata <thcdesigning@gmail.com>
2025-07-09 21:59:06 +0530

    feat: Implement multi-level menu + enque script for console logging.

--- themes/ttf-child/functions.php
+++ themes/ttf-child/functions.php
@@ -9,3 +9,9 @@ function ttfc_enqueue_styles() {
     wp_enqueue_style( 'ttfc-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version') );
 }
 add_action( 'wp_enqueue_scripts', 'ttfc_enqueue_styles' );
+
+//Enqueue a custom JavaScript file that logs a message in the browser console.
+function ttfc_enqueue_console_log_js() {
+    wp_enqueue_script( 'ttfc-console_log-js', get_stylesheet_directory_uri() . '/js/console-log.js', array(), wp_get_theme()->get('Version'), true );
+}
+add_action( 'wp_enqueue_scripts', 'ttfc_enqueue_console_log_js' );
